785f557e11d8362f17abb79d6c747f2f8b949476
staffm3 <staffm37@gmail.com>
2025-04-06 00:12:21 +0300

    task done

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,8 @@
-const getElementWidth = (content, padding, border) => {
-  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-};
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+const filterArray = (array, len) => {
+  return array.filter(i => i > len);
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
